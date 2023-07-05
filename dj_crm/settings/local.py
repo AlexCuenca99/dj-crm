8cf3f977354829d8651ac6ef38fe84ba1dc012c6
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-05 17:19:21 -0500

    Configured local database

--- dj_crm/settings/local.py
+++ dj_crm/settings/local.py
@@ -1,4 +1,4 @@
-from .base import *
+from .base import env, BASE_DIR
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -9,9 +9,25 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 # Database
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if env("DEV_STAGE") == "initial":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+elif env("DEV_STAGE") == "test":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": env("DB_NAME"),
+            "USER": env("DB_USERNAME"),
+            "PASSWORD": env("DB_PASSWORD"),
+            "HOST": env("DB_HOST"),
+            "PORT": env("DB_PORT"),
+        }
+    }
+else:
+    raise Exception(
+        "Dev stage is not configured properly. Please set initial, test or prod in .env DJANGO_DEV_STAGE."
+    )
