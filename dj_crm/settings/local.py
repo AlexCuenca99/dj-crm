e8410c19e8665507fcfe8ca88fe1ca485ad44f75
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-07-07 12:12:38 -0500

    Added email configuration

--- dj_crm/settings/local.py
+++ dj_crm/settings/local.py
@@ -39,3 +39,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+
+# Email configuration
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
