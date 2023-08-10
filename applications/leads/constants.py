248fa0e6187ef28ec62116d6c47c9c8338accb49
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-10 12:41:39 -0500

    New subject to assigned leads in creation

--- applications/leads/constants.py
+++ applications/leads/constants.py
@@ -1,1 +1,5 @@
 NEW_LEAD_CREATED_SUBJECT = "A new lead has been created. Check it out"
+
+NEW_LEAD_CREATED_AND_ASSIGNED_SUBJECT = (
+    "You have been assigned to recent created lead. Check it out"
+)
