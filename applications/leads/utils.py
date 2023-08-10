1d9729e5f46ddcba8202564baf506dce614882a8
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-09 20:46:36 -0500

    Fix lead data received

--- applications/leads/utils.py
+++ applications/leads/utils.py
@@ -6,14 +6,14 @@ from applications.utils.email.classes import TemplateEmail
 from .constants import NEW_LEAD_CREATED_SUBJECT
 
 
-def send_email_lead_created(validated_data):
-    to = validated_data["email"]
+def send_email_lead_created(lead_data):
+    to = "alextester1999@gmail.com"
     subject = NEW_LEAD_CREATED_SUBJECT
 
-    created_lead_id = validated_data["id"]
+    created_lead_id = lead_data.id
     created_lead_urls = {"lead_url": f"{settings.DOMAIN}/leads/{created_lead_id}"}
 
-    context = {"data": validated_data, "links": created_lead_urls}
+    context = {"data": lead_data, "links": created_lead_urls}
     template = "new_lead_created"
 
     template = TemplateEmail(to=to, subject=subject, template=template, context=context)
