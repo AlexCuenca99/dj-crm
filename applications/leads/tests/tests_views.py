8df609c5907b0d504bc5acab36dca8424df70b8c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-10 17:51:09 -0500

    Fix created assigned lead email sending

--- applications/leads/tests/tests_views.py
+++ applications/leads/tests/tests_views.py
@@ -17,6 +17,7 @@ class GetAllLeadsTest(APITestCase):
         self.lead_info = self.generate_lead_info()
         self.agent_info = self.generate_agent_info()
         self.organizer_info = self.generate_organizer_info()
+        self.assigned_lead_info = self.generate_assigned_lead_info()
 
     def generate_lead_info(self) -> dict:
         """Generate lead info"""
@@ -26,24 +27,38 @@ class GetAllLeadsTest(APITestCase):
             "phone": "123456789",
             "last_name": "Scott",
             "first_name": "Henry",
-            "email": "henry@crm.com",
+            "email": "henry-lead@crm.com",
             "birth": "1999-12-02",
             "gender": "M",
             "agent": "",
+        }
+
+    def generate_assigned_lead_info(self) -> dict:
+        """Generate lead info"""
+
+        return {
+            "address": "Example street",
+            "phone": "123456789",
+            "last_name": "Scott",
+            "first_name": "Henry",
+            "email": "henry-lead@crm.com",
+            "birth": "1999-12-02",
+            "gender": "M",
+            "agent": 1,
         }
 
     def generate_agent_info(self) -> dict:
         """Generate agent info"""
 
         return {
-            "email": "admin1@admin1.com",
+            "email": "alex-agent@crm.com",
             "birth": "1999-12-02",
             "first_name": "Alex",
             "last_name": "Cuenca",
             "address": "Fausto Molina",
             "phone": "0989181061",
             "gender": "F",
-            "username": "AdminAdmin",
+            "username": "AlexAgent",
             "password": "randompassword123",
             "re_password": "randompassword123",
             "role": "AGT",
@@ -53,14 +68,14 @@ class GetAllLeadsTest(APITestCase):
         """Generate organizer info"""
 
         return {
-            "email": "admin1@admin1.com",
+            "email": "alex-organizer@admin1.com",
             "birth": "1999-12-02",
             "first_name": "Alex",
             "last_name": "Cuenca",
             "address": "Fausto Molina",
             "phone": "0989181061",
             "gender": "F",
-            "username": "AdminAdmin",
+            "username": "AlexOrganizer",
             "password": "randompassword123",
             "re_password": "randompassword123",
             "role": "ORG",
@@ -89,8 +104,8 @@ class GetAllLeadsTest(APITestCase):
         # Login agent user and get token
         url = reverse("jwt-create")
         agent_login_data = {
-            "email": "admin1@admin1.com",
-            "password": "randompassword123",
+            "email": self.agent_info["email"],
+            "password": self.agent_info["password"],
         }
         response = self.client.post(url, agent_login_data)
 
@@ -115,17 +130,17 @@ class GetAllLeadsTest(APITestCase):
         response = self.client.post(url, self.organizer_info)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        agent_user = User.objects.get(username=self.agent_info["username"])
-        agent_user.is_active = True
-        agent_user.save()
+        organizer_user = User.objects.get(username=self.organizer_info["username"])
+        organizer_user.is_active = True
+        organizer_user.save()
 
         # Login agent user and get token
         url = reverse("jwt-create")
-        agent_login_data = {
-            "email": "admin1@admin1.com",
-            "password": "randompassword123",
+        organizer_login_data = {
+            "email": self.organizer_info["email"],
+            "password": self.organizer_info["password"],
         }
-        response = self.client.post(url, agent_login_data)
+        response = self.client.post(url, organizer_login_data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsNotNone(response.data["access"])
@@ -136,5 +151,46 @@ class GetAllLeadsTest(APITestCase):
         # Lead creation
         url = reverse("leads_app:leads-list")
         response = self.client.post(url, self.lead_info)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_assigned_lead(self):
+        """
+        Test for creating a single assigned lead using API
+        """
+
+        # Organizer user creation and activation
+        url = reverse("customuser-list")
+        response = self.client.post(url, self.organizer_info)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        organizer_user = User.objects.get(username=self.organizer_info["username"])
+        organizer_user.is_active = True
+        organizer_user.save()
+
+        # Login organizer user and get token
+        url = reverse("jwt-create")
+        organizer_login_data = {
+            "email": self.organizer_info["email"],
+            "password": self.organizer_info["password"],
+        }
+        response = self.client.post(url, organizer_login_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data["access"])
+
+        token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+        # Agent user creation
+        url = reverse("agents_app:agents-list")
+        agent_info = {"user": self.agent_info}
+
+        response = self.client.post(url, agent_info, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Lead creation
+        url = reverse("leads_app:leads-list")
+        response = self.client.post(url, self.assigned_lead_info)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
