e904701a5daea422577eb3a2601edc38f0015c0b
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-12 12:00:50 -0500

    Add category choices

---
+++ applications/leads/choices.py
@@ -0,0 +1,15 @@
+NEW = "NEW"
+LOST = "LST"
+ASSIGNED = "ASG"
+CONTACTED = "CNT"
+CONVERTED = "CVR"
+UNASSIGNED = "USG"
+
+
+CATEGORY_CHOICES = (
+    (NEW, "New"),
+    (LOST, "Lost"),
+    (ASSIGNED, "Assigned"),
+    (CONTACTED, "Contacted"),
+    (UNASSIGNED, "Unassigned"),
+)
