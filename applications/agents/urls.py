a0576930a562a7a3bb24957bbc70fb7e98f7579f
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-12 19:18:14 -0500

    Add custom endpoints for agents assigned leads

--- applications/agents/urls.py
+++ applications/agents/urls.py
@@ -1,5 +1,13 @@
 from django.urls import include, path
+from .views import MyAssignedLeads
 
 app_name = "agents_app"
 
-urlpatterns = [path("", include("applications.agents.routers"))]
+urlpatterns = [
+    path("", include("applications.agents.routers")),
+    path(
+        "leads/my-assigned-leads/<pk>",
+        MyAssignedLeads.as_view(),
+        name="my-assigned-leads",
+    ),
+]
