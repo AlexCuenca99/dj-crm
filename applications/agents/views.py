85c50d38db4ae86748ecd669c6c1a749e67e10ca
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-12 19:18:49 -0500

    Add view to retrieve agent assigned leads

--- applications/agents/views.py
+++ applications/agents/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from applications.leads.models import Lead
+from applications.leads.serializers import LeadModelSerializer
+from .permissions import IsAssignedOrReadOnly
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class MyAssignedLeads(generics.RetrieveUpdateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadModelSerializer
+    permission_classes = [IsAuthenticated, IsAssignedOrReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
