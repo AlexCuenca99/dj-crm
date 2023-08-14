a1f46e68ac4e98b88426f60810d2c3c63fa95277
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-14 13:36:57 -0500

    List method to my assigned leads

--- applications/agents/views.py
+++ applications/agents/views.py
@@ -8,12 +8,25 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-class MyAssignedLeads(generics.RetrieveUpdateAPIView):
+class MyAssignedLeadsRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadModelSerializer
     permission_classes = [IsAuthenticated, IsAssignedOrReadOnly]
 
-    def list(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class MyAssignedLeadsListAPIView(generics.ListAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadModelSerializer
+    permission_classes = [IsAuthenticated, IsAssignedOrReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        queryset = queryset.filter(agent_user__email=request.user_email)
+        print("QUERY_SET -> ", queryset, end="\n\n")
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
