6087b145a3768576bebbbe61f2398794bf6ee4ee
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-14 21:21:10 -0500

    Remove generic views urls

--- applications/agents/urls.py
+++ applications/agents/urls.py
@@ -1,18 +1,8 @@
 from django.urls import include, path
-from .views import MyAssignedLeadsUpdateAPIView, MyAssignedLeadsListAPIView
+from .routers import router
 
 app_name = "agents_app"
 
 urlpatterns = [
-    path("", include("applications.agents.routers")),
-    path(
-        "agents/my-assigned-leads/<pk>",
-        MyAssignedLeadsUpdateAPIView.as_view(),
-        name="my-assigned-leads-detail",
-    ),
-    path(
-        "agents/my-assigned-leads/",
-        MyAssignedLeadsListAPIView.as_view(),
-        name="my-assigned-leads-list",
-    ),
+    path("", include(router.urls)),
 ]
