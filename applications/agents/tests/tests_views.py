ff11928d79a2dd78ab2d0e081efbc567f4da7c5d
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-14 21:23:32 -0500

    Add get my leads test

--- applications/agents/tests/tests_views.py
+++ applications/agents/tests/tests_views.py
@@ -4,6 +4,7 @@ AGENT_TEST_EMAIL_ADDRESS = "mafoh16227@touchend.com"
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test.utils import override_settings
+import json
 
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -11,7 +12,6 @@ from rest_framework.test import APITestCase
 from ..models import Agent
 from ..serializers import AgentModelSerializer
 from applications.leads.models import Lead
-import json
 
 User = get_user_model()
 
@@ -208,7 +208,7 @@ class CreateSingleAgentTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
-class EditSingleAssignedLead(APITestCase):
+class EditSingleAssignedLeadTest(APITestCase):
     def setUp(self) -> None:
         """Set up a method which is used to initialize before any test run"""
         self.agent_info = self.generate_agent_info()
@@ -311,7 +311,7 @@ class EditSingleAssignedLead(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
 
         # Category Update
-        url = reverse("agents_app:my-assigned-leads", kwargs={"pk": lead.id})
+        url = reverse("agents_app:agents-my-leads", kwargs={"lead_pk": lead.id})
 
         response = self.client.patch(
             url,
@@ -358,12 +358,87 @@ class EditSingleAssignedLead(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
 
         # Category Update
-        url = reverse("agents_app:my-assigned-leads", kwargs={"pk": lead.id})
+        url = reverse("agents_app:agents-my-leads", kwargs={"lead_pk": lead.id})
 
         response = self.client.patch(
             url,
             data=json.dumps(self.valid_payload),
             content_type="application/json",
         )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetAllAssignedAgentLeadsTest(APITestCase):
+    def setUp(self) -> None:
+        self.agent_info = self.generate_agent_info()
+        self.lead_info = self.generate_lead_info()
+
+    def generate_agent_info(self) -> dict:
+        """Generate agent info"""
+        return {
+            "email": AGENT_TEST_EMAIL_ADDRESS,
+            "birth": "1999-12-02",
+            "first_name": "Alex",
+            "last_name": "Cuenca",
+            "address": "Fausto Molina",
+            "phone": "0989181061",
+            "gender": "F",
+            "username": "AlexAgent",
+            "password": "randompassword123",
+            # "re_password": "randompassword123",
+            "role": "AGT",
+        }
+
+    def generate_lead_info(self) -> dict:
+        """Generate lead info"""
+        return {
+            "address": "Example street",
+            "phone": "123456789",
+            "last_name": "Scott",
+            "first_name": "Henry",
+            "email": "henry-lead@crm.com",
+            "birth": "1999-12-02",
+            "gender": "M",
+        }
+
+    def test_get_all_assigned_agent_leads(self):
+        """Test for get all leads assigned to an agent"""
+
+        # User creation
+        user = User.objects.create_user(**self.agent_info)
+        user.is_active = True
+        user.save()
+        organization = user.user_profile
+
+        self.assertEquals(organization.id, 1)
+
+        # Agent creation
+        agent_user = Agent.objects.create(user=user, organization=organization)
+        self.assertEquals(agent_user.id, 1)
+
+        # Lead creation
+        lead = Lead.objects.create(**self.lead_info)
+        lead.agent = agent_user
+        lead.save()
+
+        self.assertEquals(lead.agent.id, 1)
+
+        # Login agent user and get token
+        url = reverse("jwt-create")
+        agent_login_data = {
+            "email": self.agent_info["email"],
+            "password": self.agent_info["password"],
+        }
+        response = self.client.post(url, agent_login_data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data["access"])
+
+        token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+        # Get all leads assigned to agent
+        url = reverse("agents_app:agents-all-my-leads")
+        response = self.client.get(url)
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
